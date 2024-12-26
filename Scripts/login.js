5c1f2484045e30d86d14ed1df39dd4c79870d0ae
Carl Mesias <carlmesias20@gmail.com>
2024-12-25 23:17:00 -0800

    Add login type
    User has option to login as Admin or Staff.
    
    Added debug dashboard.
    
    Removed admin ,doctor and nurse links from nav, only appears in dev dashboard.

---
+++ Scripts/login.js
@@ -0,0 +1,15 @@
+// Login dashboard
+
+document.addEventListener("DOMContentLoaded", function() {
+    
+    // Get refrence to submit button
+    const loginBtn = document.getElementById("login-submit");
+
+    // Add event listener to login button
+    loginBtn.addEventListener("click", function () {
+
+        // Prevent page from refreshing after submit
+        e.preventDefault();
+
+    });
+});
